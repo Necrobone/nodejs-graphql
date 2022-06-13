cd53af5fd1b7646669710f08cc03d0c5f4af2ee9
Necrobone <necrobone@hotmail.com>
2022-06-13 21:13:35 +0200

    Login with graphql

--- backend/graphql/resolvers.js
+++ backend/graphql/resolvers.js
@@ -1,6 +1,8 @@
-const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
 
 module.exports = {
   createUser: async function ({ userInput }, request) {
@@ -25,7 +27,7 @@ module.exports = {
     }
 
     if (errors.length > 0) {
-      const error = new Error('Invalid Input');
+      const error = new Error("Invalid Input");
       error.data = errors;
       error.code = 422;
       throw error;
@@ -46,5 +48,33 @@ module.exports = {
     const createdUser = await user.save();
 
     return { ...createdUser._doc, _id: createdUser._id.toString() };
+  },
+  login: async function ({ email, password }, request) {
+    const user = await User.findOne({ email });
+    if (!user) {
+      const error = new Error("User not found.");
+      error.code = 401;
+      throw error;
+    }
+
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error("Password is incorrect.");
+      error.code = 401;
+      throw error;
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        email: user.email,
+      },
+      "secret",
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    return { token, userId: user._id.toString() };
   },
 };
--- backend/graphql/schema.js
+++ backend/graphql/schema.js
@@ -19,6 +19,11 @@ module.exports = buildSchema(`
         status: String!
         posts: [Post!]!
     }
+    
+    type AuthData {
+        token: String!
+        userId: String!
+    }
 
     input UserInputData {
         email: String!
@@ -27,7 +32,7 @@ module.exports = buildSchema(`
     }
     
     type RootQuery {
-        hello: String
+        login(email: String!, password: String!): AuthData
     }
 
     type RootMutation {
